3725a3905d939ce35e460a7b153a90a6f7522374
MoisesSDelmoro <57488202+MoisesSDelmoro@users.noreply.github.com>
2021-04-11 22:14:26 -0300

    add lib mobile

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/Screens/Welcome/welcome_screen.dart';
 import 'package:flutter_auth/constants.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(MyApp());
 
